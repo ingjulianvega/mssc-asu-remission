5b83d114ae5cb8d5693182ad467597a8c86c7320
Julian Vega <ingjulianvega@gmail.com>
2021-07-30 19:30:14 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasuremission/web/model/Remission.java
+++ src/main/java/ingjulianvega/ximic/msscasuremission/web/model/Remission.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -16,8 +18,13 @@ public class Remission implements Serializable {
 
     static final long serialVersionUID = -4078429777656358572L;
 
+    @NotNull
     private UUID visitId;
+
+    @NotNull
     private UUID remissionTypeId;
+
+    @NotBlank
     private String observations;
 
 }
